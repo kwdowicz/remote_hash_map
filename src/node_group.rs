c8a84e345018d456e650527cb0b415ac7f010a00
Kamil Wdowicz <kwdowicz@gmail.com>
2024-06-14 00:44:40 +0200

    added tests

--- src/node_group.rs
+++ src/node_group.rs
@@ -28,18 +28,16 @@ impl NodeGroupRpc for ImplNodeGroupRpc {
     async fn add_server(&self, request: Request<AddServerRequest>) -> Result<Response<AddServerResponse>, Status> {
         let req = request.into_inner();
         let mut servers = self.servers.lock().await;
-        let mut result = String::from("Unknown");
         match req.addr.parse::<SocketAddr>() {
-            Ok(socket) => { servers.insert(socket.clone()); result = format!("Added: {} to cluster", socket); },
-            Err(e) => result = format!("Can't add socket to cluster: {e}"),
+            Ok(socket) => {
+                servers.insert(socket.clone());
+                Ok(Response::new(AddServerResponse { result: format!("Added {} to cluster", socket) }))
+
+            },
+            Err(e) => return Err(Status::invalid_argument(format!("Can't add socket to cluster: {e}"))),
         }
-
-        println!("{:#?}", servers);
-
-        Ok(Response::new(AddServerResponse {
-            result,
-        }))
     }
+
     async fn get_server(&self, _request: Request<GetServerRequest>) -> Result<Response<GetServerResponse>, Status> {
         let mut servers = self.servers.lock().await;
         let servers_strings: Vec<String> = servers.iter().map(|addr| addr.to_string()).collect();
@@ -67,3 +65,108 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+    use super::*;
+    use tokio::sync::Mutex;
+    use tokio::time::sleep;
+    use tonic::transport::{Server, Channel};
+    use tonic::Request;
+    use node_group_rpc::node_group_rpc_client::NodeGroupRpcClient;
+    use node_group_rpc::{AddServerRequest, GetServerRequest};
+
+    #[tokio::test]
+    async fn test_add_server() {
+        let addr = "127.0.0.1:5001".parse().unwrap();
+        let node_group_rpc = ImplNodeGroupRpc {
+            servers: Arc::new(Mutex::new(HashSet::new())),
+        };
+
+        tokio::spawn(async move {
+            Server::builder()
+                .add_service(NodeGroupRpcServer::new(node_group_rpc))
+                .serve(addr)
+                .await
+                .unwrap();
+        });
+
+        sleep(Duration::from_secs(3)).await;
+
+        let channel = Channel::from_static("http://127.0.0.1:5001")
+            .connect()
+            .await
+            .unwrap();
+        let mut client = NodeGroupRpcClient::new(channel);
+
+        let request = tonic::Request::new(AddServerRequest {
+            addr: "127.0.0.1:8080".to_string(),
+        });
+
+        let response = client.add_server(request).await.unwrap();
+        assert_eq!(response.into_inner().result, "Added 127.0.0.1:8080 to cluster");
+    }
+
+    #[tokio::test]
+    async fn test_add_server_invalid_address() {
+        let addr = "127.0.0.1:5002".parse().unwrap();
+        let node_group_rpc = ImplNodeGroupRpc {
+            servers: Arc::new(Mutex::new(HashSet::new())),
+        };
+
+        tokio::spawn(async move {
+            Server::builder()
+                .add_service(NodeGroupRpcServer::new(node_group_rpc))
+                .serve(addr)
+                .await
+                .unwrap();
+        });
+
+        sleep(Duration::from_secs(3)).await;
+
+        let channel = Channel::from_static("http://127.0.0.1:5002")
+            .connect()
+            .await
+            .unwrap();
+        let mut client = NodeGroupRpcClient::new(channel);
+
+        let request = tonic::Request::new(AddServerRequest {
+            addr: "invalid_address".to_string(),
+        });
+
+        let response = client.add_server(request).await;
+        assert!(response.is_err());
+        assert_eq!(response.err().unwrap().code(), tonic::Code::InvalidArgument);
+    }
+
+    #[tokio::test]
+    async fn test_get_server() {
+        let addr = "127.0.0.1:5003".parse().unwrap();
+        let servers = Arc::new(Mutex::new(HashSet::new()));
+        servers.lock().await.insert("127.0.0.1:8080".parse().unwrap());
+        let node_group_rpc = ImplNodeGroupRpc { servers };
+
+        tokio::spawn(async move {
+            Server::builder()
+                .add_service(NodeGroupRpcServer::new(node_group_rpc))
+                .serve(addr)
+                .await
+                .unwrap();
+        });
+
+        sleep(Duration::from_secs(3)).await;
+
+        let channel = Channel::from_static("http://127.0.0.1:5003")
+            .connect()
+            .await
+            .unwrap();
+        let mut client = NodeGroupRpcClient::new(channel);
+
+        let request = tonic::Request::new(GetServerRequest {});
+        let response = client.get_server(request).await.unwrap();
+        let result = response.into_inner().result;
+
+        assert_eq!(result.len(), 1);
+        assert_eq!(result[0], "127.0.0.1:8080");
+    }
+}
